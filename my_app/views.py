6a2e3b96b85f0126af2b693559029d97312aaa22
SidneyLi02 <66651189+SidneyLi02@users.noreply.github.com>
2020-09-19 08:01:46 -0700

    misc

--- my_app/views.py
+++ my_app/views.py
@@ -2,6 +2,25 @@ from my_app import app
 from flask import render_template, request, redirect
 import requests
 
+name = "Sidney Li"
+facts = {"Birthday":"May 18, 2002", "Favourite colour": "None"}
+posts = [{"title":"this is my title", "description": "this is my description"}]
+
+
 @app.route("/")
 def index():
-    return "Hello world!"
+    return render_template("index.html", name=name, facts=facts, posts=facts)
+
+@app.route("/change_name")
+def change_name():
+    global name
+    new_name = request.args.get("name")
+    name = new_name
+    return redirect("/")
+
+@app.route("/post", method = {"POST"})
+def make_post():
+    global posts
+    post_info = request.get_json()
+    posts.append(post_info)
+    return redirect("/")
